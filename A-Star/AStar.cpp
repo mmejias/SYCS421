79b541348984d4e26987a603a18120be9efaa678
GalacticOne <holtremington@gmail.com>
2014-02-06 16:30:34 -0500

    Added the comment detailing bug in the new main program

--- A-Star/AStar.cpp
+++ A-Star/AStar.cpp
@@ -1,3 +1,10 @@
+/* My A* Search Algorithm has an error for the tenth case in which
+ * it finds the path because the program does not print 'no path found'
+ * However, the path is not printed. I believe the error is I am not keeping a moves list
+ * and possibly only printing from the closed list.
+ * Also, I think I am destroying the path in the closed list. MUST FIX.
+*/
+
 #include "Asearch.h"
 
 int main()
